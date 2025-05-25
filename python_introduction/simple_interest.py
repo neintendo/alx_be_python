ef3f40f132085b097bf6fbb72ca1dc254318bd36
neintendo <kambashaalfredo@gmail.com>
2025-05-25 02:08:20 +0200

    added a simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+"""
+Simple Interest Calculator: A python script that calculates the simple interest earned on an investment over a period of time.
+"""
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
