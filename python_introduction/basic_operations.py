abd29824866f2b7536653c54cb2bb1ed4328d4ae
neintendo <kambashaalfredo@gmail.com>
2025-05-25 02:04:04 +0200

    added README.md & basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+"""
+Basic Operations: A python script that performs basic arithmetic operations with two predefined numbers.
+"""
+
+number1 = 10
+number2 = 5
+
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
