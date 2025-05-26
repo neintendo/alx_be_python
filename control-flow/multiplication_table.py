be0a6183ef32c8498d905310b56aeb4feb248aa5
neintendo <kambashaalfredo@gmail.com>
2025-05-26 17:42:58 +0200

    added multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,11 @@
+"""
+Multiplication Table: a python script that will ask the user to enter a number,
+                      then use a for loop to print the multiplication table for that number from 1 to 10.
+"""
+
+number = int(input("Enter a number to see its multiplication table: "))
+multiplier = 1
+
+for x in range(10):
+    print(number, "*" , multiplier, "=", number * multiplier)
+    multiplier = multiplier + 1
